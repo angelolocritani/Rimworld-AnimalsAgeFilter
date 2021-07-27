14f9b274ff78bc35b2417b42e6c430363595a66b
angelolocritani <67960642+angelolocritani@users.noreply.github.com>
2021-07-27 23:07:18 +0200

    adding files

---
+++ README.md
@@ -0,0 +1,12 @@
+# Rimworld-AnimalsAgeFilter
+ Animals' age filter mod for Rimworld
+
+![AnimalsAgeFilterPreview](https://i.imgur.com/PLWjR8V.png)
+
+![AnimalsAgeFilterExample](https://i.imgur.com/QxpG5MO.png)
+
+A simple QOL mod that allow to filter animals based on their age (baby/juvenile/adult)
+
+Steam download: https://steamcommunity.com/sharedfiles/filedetails/?id=2558978295
+
+Ludeon download: https://ludeon.com/forums/index.php?topic=54692
---
+++ Source/AnimalsAgeFilter/AnimalsAgeFilterMod.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace AnimalsAgeFilter
+{
+    public class AnimalsAgeFilterMod : Mod
+    {
+        public AnimalsAgeFilterMod(ModContentPack content) : base(content) { }
+    }
+
+    public abstract class SpecialThingFilterWorker_AllowStage : SpecialThingFilterWorker
+    {
+        private string[] targetStages;
+
+        protected SpecialThingFilterWorker_AllowStage(string[] targetStages)
+        {
+            this.targetStages = targetStages;
+        }
+
+        protected SpecialThingFilterWorker_AllowStage(string targetStage)
+        {
+            this.targetStages = new string[] { targetStage };
+        }
+
+        public override bool Matches(Thing t)
+        {
+            Pawn pawn = t as Pawn;
+            if (pawn == null)
+            {
+                return false;
+            }
+
+            foreach (string targetStage in targetStages)
+            {
+                if (pawn.ageTracker.CurLifeStage.defName == targetStage) return true;
+            }
+            return false;
+        }
+
+        public override bool CanEverMatch(ThingDef def)
+        {
+            return def.category == ThingCategory.Pawn;
+        }
+    }
+
+    public class SpecialThingFilterWorker_AllowStageBaby : SpecialThingFilterWorker_AllowStage
+    {
+        public SpecialThingFilterWorker_AllowStageBaby() : base(new string[] { "AnimalBaby", "AnimalBabyTiny" }) { }
+    }
+
+    public class SpecialThingFilterWorker_AllowStageJuvenile : SpecialThingFilterWorker_AllowStage
+    {
+        public SpecialThingFilterWorker_AllowStageJuvenile() : base("AnimalJuvenile") { }
+    }
+
+    public class SpecialThingFilterWorker_AllowStageAdult : SpecialThingFilterWorker_AllowStage
+    {
+        public SpecialThingFilterWorker_AllowStageAdult() : base("AnimalAdult") { }
+    }
+
+}
