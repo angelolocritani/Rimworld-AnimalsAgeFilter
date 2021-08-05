356848460c0804421c9752557eb7f74590e26c46
angelolocritani <67960642+angelolocritani@users.noreply.github.com>
2021-08-05 07:56:46 +0200

    Added filter for animals older than their race's life expectancy

--- Source/AnimalsAgeFilter/AnimalsAgeFilterMod.cs
+++ Source/AnimalsAgeFilter/AnimalsAgeFilterMod.cs
@@ -161,5 +161,45 @@ namespace AnimalsAgeFilter
         }
     }
 
+    public class SpecialThingFilterWorker_AllowOverLifeExpectancy : SpecialThingFilterWorker
+    {
+
+        public override bool Matches(Thing t)
+        {
+            Pawn pawn = t as Pawn;
+            if (pawn == null)
+            {
+                return false;
+            }
+
+            return pawn.ageTracker.AgeBiologicalYearsFloat >= pawn.RaceProps.lifeExpectancy;
+        }
+
+        public override bool CanEverMatch(ThingDef def)
+        {
+            return def.category == ThingCategory.Pawn;
+        }
+    }
+
+    public class SpecialThingFilterWorker_AllowNotOverLifeExpectancy : SpecialThingFilterWorker
+    {
+
+        public override bool Matches(Thing t)
+        {
+            Pawn pawn = t as Pawn;
+            if (pawn == null)
+            {
+                return false;
+            }
+
+            return pawn.ageTracker.AgeBiologicalYearsFloat < pawn.RaceProps.lifeExpectancy;
+        }
+
+        public override bool CanEverMatch(ThingDef def)
+        {
+            return def.category == ThingCategory.Pawn;
+        }
+    }
+
 
 }
