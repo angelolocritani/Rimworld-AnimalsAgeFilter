45f44433d3510d227c8374b2cc6d82c8770b4205
angelolocritani <67960642+angelolocritani@users.noreply.github.com>
2021-08-20 13:56:38 +0200

    Added carrying/not carrying filter

---
+++ Source/AnimalsAgeFilter/InventoryFilter.cs
@@ -0,0 +1,51 @@
+﻿using Verse;
+
+namespace AnimalsAgeFilter
+{
+    public class SpecialThingFilterWorker_AllowCarrying : SpecialThingFilterWorker
+    {
+
+        public override bool Matches(Thing t)
+        {
+            Pawn pawn = t as Pawn;
+            if (pawn == null)
+            {
+                return false;
+            }
+
+            return pawn.inventory.innerContainer.Count > 0;
+        }
+
+        public override bool CanEverMatch(ThingDef def)
+        {
+            return def.category == ThingCategory.Pawn && def.race.packAnimal;
+        }
+    }
+
+    public class SpecialThingFilterWorker_AllowNotCarrying : SpecialThingFilterWorker
+    {
+
+        public override bool Matches(Thing t)
+        {
+            Pawn pawn = t as Pawn;
+            if (pawn == null)
+            {
+                return false;
+            }
+
+            if (!pawn.def.race.packAnimal)
+            {
+                return true;
+            }
+            else
+            {
+                return pawn.inventory.innerContainer.Count <= 0;
+            }
+        }
+
+        public override bool CanEverMatch(ThingDef def)
+        {
+            return def.category == ThingCategory.Pawn;
+        }
+    }
+}
